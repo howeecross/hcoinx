44cb73aaba5ae1508f51aa5b5b8f23613005f793
HOWARD MOSELY <gmo.hc93@gmail.com>
2020-08-24 16:31:37 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,10 +2,10 @@ console.log('This would be the main JS file.');
 <div class="device-mockup" data-device="iphone5" data-orientation="portrait" data-color="black">
         <div class="device">
             <div class="screen">
-                <!-- Enter Our New Site-->
+                <!-- Enter Hcoin-->
             </div>
             <div class="button">
-                <!-- You can hook the "home button" to some JavaScript events or just remove it -->
+                <!-- You can hook the "purchase hcoinx" to some JavaScript events or just remove it -->
             </div>
         </div>
     </div>
